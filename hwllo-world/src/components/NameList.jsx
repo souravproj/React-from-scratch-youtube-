59ba664d10e4d9365eff77559eea9144f59b4fa0
souravproj <souravkundu1920@gmail.com>
2023-03-21 23:42:53 +0530

    updated

--- hwllo-world/src/components/NameList.jsx
+++ hwllo-world/src/components/NameList.jsx
@@ -2,6 +2,7 @@ import React from 'react'
  import Person from './Person'
 
 function NameList() {
+    const names=['Sourav','Sampurna','Sathi']//if you can give here any kinds of same value then it will be throw error in console
     const persons=[
         {
             id:1,
@@ -22,13 +23,17 @@ function NameList() {
             skill:'Angular'
         }
     ]
-    const personList=persons.map(person=><Person key={person.name} person={person}/>)
-    return <div>{personList}</div>
+
+    const nameList=names.map((name,index)=><h2 key={index}>{index} {name}</h2>)
+    return <div>{nameList}</div>
+
+// const personList=persons.map(person=><Person key={person.name} person={person}/>)
+// return <div>{personList}</div>
     
-//     const personList=persons.map(person=><h2>I am {person.name} and My age is {person.age} and My id is {person.id}</h2>)
-//   return (
+// const personList=persons.map(person=><h2>I am {person.name} and My age is {person.age} and My id is {person.id}</h2>)
+// return (
 //     <div>{personList}</div>
- // )
+//  )
 }
 
 export default NameList
