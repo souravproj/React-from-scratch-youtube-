14da07276d84732b1a32e912471769ae0c31b4a5
souravproj <souravkundu1920@gmail.com>
2023-03-23 00:14:59 +0530

    update

--- hwllo-world/src/components/Form.js
+++ hwllo-world/src/components/Form.js
@@ -6,7 +6,8 @@ import React, { Component } from 'react'
      
        this.state = {
           username:'',
-          comments:''
+          comments:'',
+          topic:'React'
        }
      }
 
@@ -21,9 +22,20 @@ import React, { Component } from 'react'
              comments:event.target.value
          })
      }
+     hadleTopicChange=(event)=>{
+         this.setState({
+             topic:event.target.value
+         })
+     }
+
+     handleSubmit=(event)=>{
+        alert(`${this.state.username} ${this.state.comments} ${this.state.topic}`)
+        event.preventDefault()
+     }
   render() {
+    //   const{username,comments,topic}=this.state(if we can use this line then we dont have to write `thus.state` ,we can write only necessary one)
     return (
-        <form>
+        <form onSubmit={this.handleSubmit}>
             <div>
                  <label>Username</label>
                  <input type='text' value={this.state.username} onChange={this.handleUsernameChange}/>
@@ -32,6 +44,15 @@ import React, { Component } from 'react'
                 <label>Comments</label>
                 <textarea value={this.state.comments} onChange={this.handleCommentsChange}></textarea>
             </div>
+            <div>
+                <label>Topic</label>
+                <select value={this.state.topic} onChange={this.hadleTopicChange}>
+                    <option value="React">React</option>
+                    <option value="Angular">Angular</option>
+                    <option value="vue">vue</option>
+                </select>
+            </div>
+            <button type='submit' >Submit</button>
         </form>
       
     )
